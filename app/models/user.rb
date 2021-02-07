4009f169f21f49c382a9d3583f81e6f6442752d6
momoyama867 <77099609+momoyama867@users.noreply.github.com>
2021-02-07 21:47:23 +0900

    rubocopによるコード整形

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,8 @@ class User < ApplicationRecord
     validates :birth_date
   end
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 
   with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'には全角（漢字・ひらがな・カタカナ）を使用してください' } do
     validates :last_name
