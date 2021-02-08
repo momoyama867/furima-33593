906108ef5a7ee803d7e3e56567b92116ef27b2cb
momoyama867 <77099609+momoyama867@users.noreply.github.com>
2021-02-08 14:20:14 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,26 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :category_id
+    validates :condition_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :days_to_ship_id
+    validates :price
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :days_to_ship_id
+  end
+
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than: 10000000 }
 end
