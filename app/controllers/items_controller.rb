14d837ff16af7d851176c2d44e3aa9d5ac1a9521
momoyama867 <77099609+momoyama867@users.noreply.github.com>
2021-02-09 16:14:17 +0900

    並び替えにorderメソッドを使用

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,8 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @items = Item.all
-    @items = @items.sort_by(&:id).reverse
+    @items = Item.order("created_at DESC")
   end
 
   def new
